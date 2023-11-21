6bd115e5584a9eb065406a574da8348d41ad520d
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2023-11-21 16:49:34 +0100

    [MLIR] Change `sparse-compiler` to `sparsifier`

--- sparsifier.py
+++ sparsifier.py
@@ -11,7 +11,7 @@ from typing import Sequence
 class Sparsifier:
 
     def __init__(self, options: str, opt_level: int, shared_libs: Sequence[str]):
-        pipeline = f"builtin.module(sparse-compiler{{{options} reassociate-fp-reductions=1 enable-index-optimizations=1}})"
+        pipeline = f"builtin.module(sparsifier{{{options} reassociate-fp-reductions=1 enable-index-optimizations=1}})"
         self.pipeline = pipeline
         self.opt_level = opt_level
         self.shared_libs = shared_libs
