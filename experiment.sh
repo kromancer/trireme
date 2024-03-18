482f5ba85f6f58c612c5d970bb3e33fdc7d30120
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-03-18 14:56:52 +0100

    [experiment] Collect profiling data for baselines, scale input

---
+++ experiment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for rows in {1..500}; do
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py benchmark -r $rows -c 2000000000 -d 0.0005 --repetitions 10
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py profile toplev -r $rows -c 2000000000 -d 0.0005
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py profile events -r $rows -c 2000000000 -d 0.0005
+
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py benchmark -o pref-ains -r $rows -c 2000000000 -d 0.0005 --repetitions 10 -pd 64
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py profile toplev -o pref-ains -r $rows -c 2000000000 -d 0.0005 -pd 64
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py profile events -o pref-ains -r $rows -c 2000000000 -d 0.0005 -pd 64
+done
