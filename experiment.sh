3dfc18fe8c351bd8f6544bc701f5ffbeb5616b1a
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-03-20 13:30:54 +0100

    [experiment] Run pref-ains -r 1..200 -pd 64 -l 2

--- experiment.sh
+++ experiment.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-cd /trireme
-
-source venv/bin/activate
-
-python spmv.py benchmark -r 100 -c 2000000000 -d 0.0005  --repetitions 10
-
-for pd in {20..5000}; do
-
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -r 100 -c 2000000000 -d 0.0005 -pd $pd --repetitions 10
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-2 -r 100 -c 2000000000 -d 0.0005 -pd $pd --repetitions 10
-
+for rows in {1..200}; do
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py benchmark -o pref-ains -r $rows -c 2000000000 -d 0.0005 --repetitions 10 -pd 64 -l 2
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py profile toplev -o pref-ains -r $rows -c 2000000000 -d 0.0005 -pd 64 -l 2
+    taskset -c 0,1,2,3,4,5,6,7 python spmv.py profile events -o pref-ains -r $rows -c 2000000000 -d 0.0005 -pd 64 -l 2
 done
