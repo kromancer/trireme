a5d5168b202af45c40ace88a072f5e5d96911c6d
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-03-21 20:47:31 +0100

    [experiment] Run omp-tasks-2 with `PREFETCHT2`

--- experiment.sh
+++ experiment.sh
@@ -7,11 +7,8 @@ cd /trireme
 
 source venv/bin/activate
 
-python spmv.py benchmark -r $ROWS -c $COLS -d 0.0005  --repetitions 10
+for PD in {60..300}; do
 
-for PD in {114..500}; do
-
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-2 -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
+    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-2 -r $ROWS -c $COLS -d 0.0005 -pd $PD -l 2 --repetitions 5
 
  done
