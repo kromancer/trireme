7902024415326c53d2550f2fe1db631fb8d5e60b
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-03-21 10:19:30 +0100

    [experiment] Run omp-tasks vs omp-tasks-v2 but remove small pds

--- experiment.sh
+++ experiment.sh
@@ -9,9 +9,9 @@ source venv/bin/activate
 
 python spmv.py benchmark -r $ROWS -c $COLS -d 0.0005  --repetitions 10
 
-for PD in {35..5000}; do
+for PD in {114..500}; do
 
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 10
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-2 -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 10
+    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
+    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-2 -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
 
  done
