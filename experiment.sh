1f0e5e9edbf3c6d022c35191365f2f718e07323a
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-03-27 22:36:30 +0100

    [experiment] v1 - v3 - v4

--- experiment.sh
+++ experiment.sh
@@ -7,8 +7,15 @@ cd /trireme
 
 source venv/bin/activate
 
-for PD in {60..300}; do
+python spmv.py benchmark -r $ROWS -c $COLS -d 0.0005 --repetitions 5
 
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-2 -r $ROWS -c $COLS -d 0.0005 -pd $PD -l 2 --repetitions 5
+for PD in {35..300}; do
+
+    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks -r $ROWS -c $COLS -d 0.0005 -pd $PD -l 3 --repetitions 5
+
+    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-3 -r $ROWS -c $COLS -d 0.0005 -pd $PD -l 3 --repetitions 5
+    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-3 -r $ROWS -c $COLS -d 0.0005 -pd $PD -l 2 --repetitions 5
+
+    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-4 -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
 
  done
