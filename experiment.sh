5df9e3a582faf30281b7ba84926765e3393f79c1
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-03-28 15:35:42 +0100

    [experiment] Find out the cost of having a dummy pref task with no pref

--- experiment.sh
+++ experiment.sh
@@ -7,15 +7,8 @@ cd /trireme
 
 source venv/bin/activate
 
-python spmv.py benchmark -r $ROWS -c $COLS -d 0.0005 --repetitions 5
-
 for PD in {35..300}; do
 
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks -r $ROWS -c $COLS -d 0.0005 -pd $PD -l 3 --repetitions 5
-
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-3 -r $ROWS -c $COLS -d 0.0005 -pd $PD -l 3 --repetitions 5
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-3 -r $ROWS -c $COLS -d 0.0005 -pd $PD -l 2 --repetitions 5
-
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-4 -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
+    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-dummy -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
 
  done
