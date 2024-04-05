23b9ed41008bc663d8ba527d66b65639645eeb45
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-04-05 21:38:33 +0200

    [create_sparse_mats.py] Randomize vector

--- create_sparse_mats.py
+++ create_sparse_mats.py
@@ -8,10 +8,11 @@ from utils import print_size
 
 def create_sparse_mat_and_dense_vec(rows: int, cols: int, density: float, form: str = "coo") -> Tuple[Union[sp.coo_array, sp.csr_array], np.ndarray]:
 
-    dense_vec = np.array(cols * [1.0], np.float64)
+    rng = np.random.default_rng(5)
+
+    dense_vec = rng.random(cols)
     print(f"vector: size: {print_size(cols * np.float64().itemsize)}")
 
-    rng = np.random.default_rng(5)
     sparse_mat = sp.random_array((rows, cols), density=density, dtype=np.float64, format=form, random_state=rng)
     print(f"sparse matrix: non-zero values size: {print_size(rows * cols * density * np.float64().itemsize)}, "
           f"density: {density}%")
