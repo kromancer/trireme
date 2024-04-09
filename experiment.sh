de67796f982f0b9df18aa76dd74a66464bac4ef1
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-04-09 14:49:57 +0200

    [experiment] Run multistage, l1-mshrs in 1,10 and l2-mshrs 1,100

--- experiment.sh
+++ experiment.sh
@@ -3,11 +3,12 @@
 ROWS=2000
 COLS=150000000
 
-python spmv.py benchmark -r $ROWS -c $COLS -d 0.0005 --repetitions 5
+source /home/paul/venv/bin/activate
 
-for PD in {35..300}; do
+python spmv.py benchmark -r $ROWS -c $COLS -d 0.0005 --repetitions 8
 
-    python spmv.py benchmark -o pref-ains -r $ROWS -c $COLS -d 0.0005 --repetitions 5
-    python spmv.py benchmark -o pref-spe  -r $ROWS -c $COLS -d 0.0005 --repetitions 5
-
- done
+for L1_MSHRS in {1..10}; do
+    for L2_MSHRS in {1..100}; do
+        python spmv_multistage.py benchmark -r $ROWS -c $COLS --l1-mshrs "$L1_MSHRS" --l2-mshrs "$L2_MSHRS" -d 0.0005 --repetitions 8
+    done
+done
