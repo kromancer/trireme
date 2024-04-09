8d41937b97a2de5651672ae457d60620df59724e
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-04-09 10:53:27 +0200

    [experiment] Run baselines on e-cores

--- experiment.sh
+++ experiment.sh
@@ -3,13 +3,11 @@
 ROWS=2000
 COLS=150000000
 
-cd /trireme
-
-source venv/bin/activate
+python spmv.py benchmark -r $ROWS -c $COLS -d 0.0005 --repetitions 5
 
 for PD in {35..300}; do
 
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-5 -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
-    OMP_PROC_BIND=spread OMP_PLACES=cores python spmv_runahead.py benchmark -v omp-tasks-5-dummy -r $ROWS -c $COLS -d 0.0005 -pd $PD --repetitions 5
+    python spmv.py benchmark -o pref-ains -r $ROWS -c $COLS -d 0.0005 --repetitions 5
+    python spmv.py benchmark -o pref-spe  -r $ROWS -c $COLS -d 0.0005 --repetitions 5
 
  done
