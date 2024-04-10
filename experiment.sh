a26f1e16157230adc3a6235010e819586fc5d90a
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-04-10 14:44:15 +0200

    [experiment] multistage: l1:l2 mshrs 1-15:1-256, with/without l1 hw pref

--- experiment.sh
+++ experiment.sh
@@ -7,8 +7,14 @@ source /home/paul/venv/bin/activate
 
 python spmv.py benchmark -r $ROWS -c $COLS -d 0.0005 --repetitions 8
 
-for L1_MSHRS in {1..10}; do
-    for L2_MSHRS in {1..100}; do
+for L1_MSHRS in {1..15}; do
+    for L2_MSHRS in {1..256}; do
         python spmv_multistage.py benchmark -r $ROWS -c $COLS --l1-mshrs "$L1_MSHRS" --l2-mshrs "$L2_MSHRS" -d 0.0005 --repetitions 8
+    done
+done
+
+for L1_MSHRS in {1..15}; do
+    for L2_MSHRS in {1..256}; do
+        python spmv_multistage.py benchmark -r $ROWS -c $COLS --l1-mshrs "$L1_MSHRS" --l2-mshrs "$L2_MSHRS" --disable-l1-ipp --disable-l1-npp -d 0.0005 --repetitions 8
     done
 done
