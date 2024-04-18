f2db9429db906889637d1996b1292900778d522c
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-04-18 23:31:41 +0200

    [experiment] Disable l1-ipp -> l1-npp -> l2-stream -> l2-amp -> llc-stream

--- experiment.sh
+++ experiment.sh
@@ -18,3 +18,11 @@ done
 for L2_MSHRS in {20..80}; do
     python spmv_multistage.py benchmark -r $ROWS -c $COLS --l1-mshrs 10 --l2-mshrs "$L2_MSHRS" -d 0.0005 --disable-l1-ipp --disable-l1-npp --disable-l2-stream --repetitions 8
 done
+
+for L2_MSHRS in {20..80}; do
+    python spmv_multistage.py benchmark -r $ROWS -c $COLS --l1-mshrs 10 --l2-mshrs "$L2_MSHRS" -d 0.0005 --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --repetitions 8
+done
+
+for L2_MSHRS in {20..80}; do
+    python spmv_multistage.py benchmark -r $ROWS -c $COLS --l1-mshrs 10 --l2-mshrs "$L2_MSHRS" -d 0.0005 --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream --repetitions 8
+done
