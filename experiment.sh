dbd981107968d060406d89c4836aa0e8a4689eab
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-04-27 20:59:57 +0200

    [experiment] Profile multistage for L1_MSHRS in {5,10,15,20}

--- experiment.sh
+++ experiment.sh
@@ -2,10 +2,13 @@
 
 ROWS=2000
 COLS=150000000
-L1_MSHRS=15
 
 source /home/paul/venv/bin/activate
 
-for L2_MSHRS in {1..128}; do
-    python spmv_multistage.py profile memory-access -r $ROWS -c $COLS --l1-mshrs "$L1_MSHRS" --l2-mshrs "$L2_MSHRS" --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream -d 0.0005
+for L1_MSHRS in 5 10 15 20; do
+    for L2_MSHRS in {1..150}; do
+        python spmv_multistage.py benchmark -r $ROWS -c $COLS --l1-mshrs "$L1_MSHRS" --l2-mshrs "$L2_MSHRS" --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream -d 0.0005 --repetitions 10
+        python spmv_multistage.py profile uarch-exploration -r $ROWS -c $COLS --l1-mshrs "$L1_MSHRS" --l2-mshrs "$L2_MSHRS" --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream -d 0.0005
+        python spmv_multistage.py profile memory-access -r $ROWS -c $COLS --l1-mshrs "$L1_MSHRS" --l2-mshrs "$L2_MSHRS" --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream -d 0.0005
+    done
 done
