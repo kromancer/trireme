eefe06ad7dd92ae68d6fa630eb8261e4474b4d0a
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-09-27 11:14:18 +0200

    [experiment.sh] Run SparseSuite on E-Core with HW pref

--- experiment.sh
+++ experiment.sh
@@ -2,6 +2,14 @@
 
 source /home/paul/venv/bin/activate
 
+# spmv coo on SparseSuite on E-core 8
+
+# Benchmark with HW prefetching
+python spmv_on_suitesparse.py --matrix-format coo benchmark --repetitions 10
+python spmv_on_suitesparse.py -o pref-ains --matrix-format coo benchmark --repetitions 10
+python spmv_on_suitesparse.py -o pref-mlir --matrix-format coo benchmark --repetitions 10
+
+# Profile with HW prefetching
 python spmv_on_suitesparse.py --matrix-format coo profile events
 python spmv_on_suitesparse.py -o pref-ains --matrix-format coo profile events
 python spmv_on_suitesparse.py -o pref-mlir --matrix-format coo profile events
