7ceddd528e784ded18762b397d94746ab9bf3596
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-10-29 16:00:10 +0100

    [experiment.sh] Repeat on e-core for missing

--- experiment.sh
+++ experiment.sh
@@ -4,13 +4,7 @@ source /home/paul/venv/bin/activate
 
 # spmv coo on SparseSuite on E-core 8
 
-# Profile with HW prefetching
-python spmv_on_suitesparse.py -o pref-ains --matrix-format coo profile events
-python spmv_on_suitesparse.py -o pref-mlir --matrix-format coo profile events
-python spmv_on_suitesparse.py --matrix-format coo profile events
-
 # Benchmark with HW prefetching
-python spmv_on_suitesparse.py --matrix-format coo benchmark --repetitions 10
-python spmv_on_suitesparse.py -o pref-ains --matrix-format coo benchmark --repetitions 10
-python spmv_on_suitesparse.py -o pref-mlir --matrix-format coo benchmark --repetitions 10
-
+# python spmv_on_suitesparse.py --matrix-format coo benchmark --repetitions 10
+python spmv_on_suitesparse.py -c pref_ains_benchmark -o pref-ains --matrix-format coo benchmark --repetitions 10
+python spmv_on_suitesparse.py -c pref_mlir_benchmark -o pref-mlir --matrix-format coo benchmark --repetitions 10
