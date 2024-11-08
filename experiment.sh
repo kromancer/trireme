8949da769cc9a9ef5dbba9a849973ce27abfc043
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-11-08 17:54:51 +0100

    [experiment.sh] SuiteSparse on E-core with L2 and LLC HW prefetching off

--- experiment.sh
+++ experiment.sh
@@ -4,8 +4,12 @@ source /home/paul/venv/bin/activate
 
 # spmv coo on SparseSuite on E-core 8
 
-# Profile with HW prefetching on
-python spmv_on_suitesparse.py -c no-opt-prof --matrix-format coo profile events
+# Profile with L2 and LLC hw pref off
+python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream --matrix-format coo profile events
+python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-ains --matrix-format coo profile events
+python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-mlir --matrix-format coo profile events
 
-# Benchmark with HW prefetching on
-python spmv_on_suitesparse.py -c no-opt-bench --matrix-format coo benchmark --repetitions 10
+# Benchmark with L2 and LLC hw pref off
+python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream --matrix-format coo benchmark --repetitions 10
+python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-ains --matrix-format coo benchmark --repetitions 10
+python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-mlir --matrix-format coo benchmark --repetitions 10
