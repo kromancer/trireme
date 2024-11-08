52b5422768955e8bbef1e797663bece1c3ab2ed1
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-11-08 10:25:42 +0100

    [experiment.sh] Run missing ss matrices for e-core with hw pref on

--- experiment.sh
+++ experiment.sh
@@ -4,12 +4,8 @@ source /home/paul/venv/bin/activate
 
 # spmv coo on SparseSuite on E-core 8
 
-# Profile with HW prefetching off
-python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream --matrix-format coo profile events
-python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-ains --matrix-format coo profile events
-python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-mlir --matrix-format coo profile events
+# Profile with HW prefetching on
+python spmv_on_suitesparse.py -c no-opt-prof --matrix-format coo profile events
 
-# Benchmark with HW prefetching off
-python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream --matrix-format coo benchmark --repetitions 10
-python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-ains --matrix-format coo benchmark --repetitions 10
-python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l1-npp --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-mlir --matrix-format coo benchmark --repetitions 10
+# Benchmark with HW prefetching on
+python spmv_on_suitesparse.py -c no-opt-bench --matrix-format coo benchmark --repetitions 10
