0885e428e01ebd75bf7352b759df27bfd7843d20
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-11-12 10:17:02 +0100

    [experiment.sh] Prof & bench spmv on SS coo with just the L1 NPP

--- experiment.sh
+++ experiment.sh
@@ -4,12 +4,17 @@ source /home/paul/venv/bin/activate
 
 # spmv coo on SparseSuite on E-core 8
 
-# Profile with L2 and LLC hw pref off
-python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream --matrix-format coo profile events
-python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-ains --matrix-format coo profile events
-python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-mlir --matrix-format coo profile events
+# Profile with L1 IPP, L2 and LLC hw pref off
+python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l2-stream --disable-l2-amp --disable-llc-stream --matrix-format coo profile events
+python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-ains --matrix-format coo profile events
+python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-mlir --matrix-format coo profile events
 
-# Benchmark with L2 and LLC hw pref off
-python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream --matrix-format coo benchmark --repetitions 10
-python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-ains --matrix-format coo benchmark --repetitions 10
-python spmv_on_suitesparse.py -c all --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-mlir --matrix-format coo benchmark --repetitions 10
+# Benchmark with L1 IPP, L2 and LLC hw pref off
+python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l2-stream --disable-l2-amp --disable-llc-stream --matrix-format coo benchmark --repetitions 10
+python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-ains --matrix-format coo benchmark --repetitions 10
+python spmv_on_suitesparse.py -c all --disable-l1-ipp --disable-l2-stream --disable-l2-amp --disable-llc-stream -o pref-mlir --matrix-format coo benchmark --repetitions 10
+
+# Profile with hw pref on to get the LD_HEAD.* counters
+python spmv_on_suitesparse.py -c all --matrix-format coo profile events
+python spmv_on_suitesparse.py -c all -o pref-ains --matrix-format coo profile events
+python spmv_on_suitesparse.py -c all -o pref-mlir --matrix-format coo profile events
