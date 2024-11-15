f6000d6b77365bbb983c99848d555aa8325252fa
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2024-11-15 11:41:22 +0100

    [spmv_on_suitesparse.py] Fix bug where HW pref ctrl args were ignored

--- spmv_on_suitesparse.py
+++ spmv_on_suitesparse.py
@@ -2,30 +2,21 @@ import argparse
 import json
 from pathlib import Path
 from subprocess import run, DEVNULL
+from typing import List, Tuple
+
 from tqdm import tqdm
 
 from common import read_config
 from suite_sparse import get_all_suitesparse_matrix_names
-
-from argument_parsers import add_args_for_benchmark, add_output_check_arg, add_sparse_format_arg
-from hwpref_controller import HwprefController
 
 
 def main():
     script_dir = Path(__file__).parent.resolve()
     cfg_file = script_dir / "suite-sparse-config.json"
 
-    args = parse_args(cfg_file)
+    args, unknown_args = parse_args(cfg_file)
 
-    opt = ["-o", args.optimization] if args.optimization else []
-    check_output = ["--check-output"] if args.check_output else []
-
-    command = ["python", "spmv.py", *check_output, *opt, "--matrix-format", f"{args.matrix_format}"]
-
-    if args.action == "profile":
-        command += [args.action, args.analysis]
-    else:
-        command += ["benchmark", "--repetitions", "10"]
+    command = ["python", "spmv.py"] + unknown_args
 
     if args.collection == "all":
         matrix_names = set(get_all_suitesparse_matrix_names(is_real=True))
@@ -40,9 +31,10 @@ def main():
             pbar.update(1)
 
 
-def parse_args(cfg_file: Path) -> argparse.Namespace:
+def parse_args(cfg_file: Path) -> Tuple[argparse.Namespace, List[str]]:
     parser = argparse.ArgumentParser(description="(Sparse Matrix)x(Dense Vector) Multiplication (SpMV) with MLIR on "
-                                                 "SuiteSparse matrices.")
+                                                 "SuiteSparse matrices.",
+                                     epilog="Any arguments not listed above will be forwarded as-is to spmv.py.")
     cfg = {}
     try:
         with open(cfg_file, "r") as f:
@@ -52,32 +44,13 @@ def parse_args(cfg_file: Path) -> argparse.Namespace:
     except json.decoder.JSONDecodeError as e:
         print(f"{cfg_file} could not be decoded {e}")
 
-    add_output_check_arg(parser)
     parser.add_argument("-c", "--collection",
                         choices=list(cfg.keys()) + ["all"],
                         help="Specify the collection of SuiteSparse matrices to use for SpMV. "
                              "Choose from predefined collections in "
                              f"{cfg_file}, or use 'all' to run on any matrix that is not in 'exclude-from-all'.")
-    parser.add_argument("-o", "--optimization",
-                        choices=["vect-vl4", "pref-mlir", "pref-ains", "pref-spe"],
-                                   help="Use an optimized version of the kernel")
-    add_sparse_format_arg(parser, "matrix")
 
-    HwprefController.add_args(parser)
-
-    # 1st level subparsers, benchmark or profile
-    action_subparser = parser.add_subparsers(dest="action", help="Choose action: benchmark or profile")
-
-    # Subcommand: benchmark
-    benchmark_parser = action_subparser.add_parser("benchmark", help="Benchmark the application")
-    add_args_for_benchmark(benchmark_parser)
-
-    # Subcommand: profile
-    profile_parser = action_subparser.add_parser("profile", help="Profile the application")
-    profile_parser.add_argument("analysis", choices=["toplev", "vtune", "events"],
-                                help="Choose an analysis type")
-
-    return parser.parse_args()
+    return parser.parse_known_args()
 
 
 if __name__ == "__main__":
