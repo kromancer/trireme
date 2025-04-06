917c67276e535d2a97e537789f4dcaafde7445b8
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2025-04-06 23:39:27 +0200

    [results] Add speed-ups for spmv after isolation

--- results/compute_speedups.py
+++ results/compute_speedups.py
@@ -9,19 +9,20 @@ if __name__ == "__main__":
     argparser.add_argument("-o", "--optimized", type=str, required=True)
     args = argparser.parse_args()
 
-    opt = Path(args.optimized)
-    assert opt.exists(), f"{opt} does not exist"
-    with open(args.baseline, "r") as b, open(opt, "r") as o, open(opt.parent / ("bak-" + opt.name), "w") as bak:
+    base_file = Path(args.baseline)
+    assert base_file.exists(), f"{base_file} does not exist"
+    optimized_file = Path(args.optimized)
+    assert optimized_file.exists(), f"{optimized_file} does not exist"
+    with open(base_file, "r") as b, open(optimized_file, "r") as o, open(optimized_file.parent / ("bak-" + optimized_file.name), "w") as bak:
         baseline = json.load(b)
         optimized = json.load(o)
         bak.write(json.dumps(optimized, indent=4))
 
-    new_data = {}
     for mtx, v in optimized.items():
-        new_data[mtx] = v
         time_field = "time_ms" if "time_ms" in v else "mean_ms"
-        new_data[mtx]["speed-up"] = baseline[mtx][time_field] / v[time_field]
+        v.setdefault("speed-ups", {})
+        v["speed-ups"][str(base_file.resolve())] = baseline[mtx][time_field] / v[time_field]
 
-    new_data = dict(sorted(new_data.items(), key=lambda e: e[1]["speed-up"], reverse=True))
-    with open(opt, "w") as f:
+    new_data = dict(sorted(optimized.items(), key=lambda e: e[1]["speed-ups"][str(base_file.resolve())], reverse=True))
+    with open(optimized_file, "w") as f:
         f.write(json.dumps(new_data, indent=4))
