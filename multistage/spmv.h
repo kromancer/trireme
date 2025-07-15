33339d6e736b07b7b24267f371647baedb5aa723
Konstantinos Sotiropoulos <konstantinos.sotiropoulos@chalmers.se>
2025-07-15 09:42:21 +0200

    Remove old project ideas

--- multistage/spmv.h
+++
@@ -1,9 +0,0 @@
-#ifndef SPMV_H
-#define SPMV_H
-
-#include <stdint.h>
-
-double compute(uint64_t num_of_rows, const double *vec, const double *mat_vals, const int64_t *pos, const int64_t *crd,
-               double *res);
-
-#endif //SPMV_RUNAHEAD_WITH_OMP_TASKS_H
